7fdf28ebab803661620956066628a8b0d2e40bd8
devfle <hello@flesch-design.de>
2022-02-09 16:02:47 +0100

    refactor: remove not needed spread operator

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,9 @@
  const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
 
  module.exports = {
-     ...defaultConfig,
-     ...{
-         entry: {
-             "ep-frontend": path.resolve( process.cwd(), 'src', 'ep-frontend.js' ),
-             "index": path.resolve( process.cwd(), 'src', 'index.js' ),
-         },
-     }
+    ...defaultConfig,
+    entry: {
+        "ep-frontend": path.resolve( process.cwd(), 'src', 'ep-frontend.js' ),
+        "index": path.resolve( process.cwd(), 'src', 'index.js' ),
+    },
  }
